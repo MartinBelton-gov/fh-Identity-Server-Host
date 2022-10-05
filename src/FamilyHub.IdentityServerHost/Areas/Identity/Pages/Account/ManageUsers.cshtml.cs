130640763149fa5f890c4bc74810af0becf5e195
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-10-05 16:55:41 +0100

    Added Reset Password

--- src/FamilyHub.IdentityServerHost/Areas/Identity/Pages/Account/ManageUsers.cshtml.cs
+++ src/FamilyHub.IdentityServerHost/Areas/Identity/Pages/Account/ManageUsers.cshtml.cs
@@ -1,10 +1,13 @@
 using FamilyHub.IdentityServerHost.Models;
-using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralTaxonomys;
 using FamilyHubs.SharedKernel;
-using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text;
 
 namespace FamilyHub.IdentityServerHost.Areas.Identity.Pages.Account;
 
@@ -12,6 +15,7 @@ public class ManageUsersModel : PageModel
 {
     
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly IEmailSender _emailSender;
 
     [BindProperty]
     public int PageNumber { get; set; } = 1;
@@ -20,18 +24,17 @@ public class ManageUsersModel : PageModel
 
     public int TotalPages { get; set; } = 1;
 
+    [BindProperty]
+    public string Search { get; set; } = default!;
+
     public PaginatedList<ApplicationUser> Users { get; set; } = new PaginatedList<ApplicationUser>();
     public string ReturnUrl { get; set; } = default!;
 
-    public ManageUsersModel(UserManager<IdentityUser> userManager)
+    public ManageUsersModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
     {
         _userManager = userManager;       
+        _emailSender = emailSender;
     }
-
-    //public async Task OnGet()
-    //{
-    //    await GetPage();
-    //}
 
     public async Task OnGet(string pageNumber)
     {
@@ -57,6 +60,11 @@ public class ManageUsersModel : PageModel
         await GetPage();
     }
 
+    public async Task OnPost()
+    {
+        await GetPage();
+    }
+
     private async Task GetPage()
     {
         ReturnUrl ??= Url.Content("~/Identity/Account/ManageUsers");
@@ -76,8 +84,46 @@ public class ManageUsersModel : PageModel
             });
         }
 
-        var pagelist = applicationUsers.Skip((PageNumber - 1) * PageSize).Take(PageSize).ToList();
-        Users = new PaginatedList<ApplicationUser>(pagelist, pagelist.Count, PageNumber, PageSize);
-        TotalPages = applicationUsers.Count / PageSize;
+        //filter depending on user
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+        List<ApplicationUser> pagelist;
+        if (!string.IsNullOrEmpty(Search))
+        {
+            var allPages = applicationUsers.Where(x => x.UserName.Contains(Search) || x.Email.Contains(Search) || x.Roles.Contains(Search));
+            pagelist = allPages.Skip((PageNumber - 1) * PageSize).Take(PageSize).ToList();
+            TotalPages = (int)Math.Ceiling((double)allPages.Count() / (double)PageSize);
+            Users = new PaginatedList<ApplicationUser>(pagelist, allPages.Count(), PageNumber, PageSize);
+        }
+        else
+        {
+            pagelist = applicationUsers.Skip((PageNumber - 1) * PageSize).Take(PageSize).ToList();
+            TotalPages = (int)Math.Ceiling((double)applicationUsers.Count / (double)PageSize);
+            Users = new PaginatedList<ApplicationUser>(pagelist, pagelist.Count, PageNumber, PageSize);
+        }
+    }
+
+    public async Task<IActionResult> OnPostResetPassword(string id)
+    {
+        var user = await _userManager.FindByEmailAsync(id);
+        if (user == null)
+            return Page();
+
+        // For more information on how to enable account confirmation and password reset please
+        // visit https://go.microsoft.com/fwlink/?LinkID=532713
+        var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+        var callbackUrl = Url.Page(
+            "/Account/ResetPassword",
+            pageHandler: null,
+            values: new { area = "Identity", code },
+            protocol: Request.Scheme);
+
+        await _emailSender.SendEmailAsync(
+            user.Email,
+            "Reset Password",
+            $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+        return RedirectToPage("./ForgotPasswordConfirmation");
     }
 }
